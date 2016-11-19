f8f01a44e7cd7e28ebc45a344287e339deda7187
parkerwy <parkerwy@gmail.com>
2016-11-19 20:04:54 +0800

    created simple spring boot app running in liberty.

---
+++ src/main/java/lab/heisenbug/sandbox/SandboxApplication.java
@@ -0,0 +1,21 @@
+package lab.heisenbug.sandbox;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by parker on 19/11/2016.
+ */
+@SpringBootApplication
+public class SandboxApplication extends SpringBootServletInitializer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SandboxApplication.class);
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        LOGGER.info("building spring boot application.");
+        return builder.sources(SandboxApplication.class);
+    }
+}
