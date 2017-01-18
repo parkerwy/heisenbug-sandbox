0baca1e3227a7859139f25d0927c6c525c556eb6
parkerwy <parkerwy@gmail.com>
2017-01-19 00:35:57 +0800

    include git info and build info.

--- src/main/java/lab/heisenbug/sandbox/SandboxApplication.java
+++ src/main/java/lab/heisenbug/sandbox/SandboxApplication.java
@@ -2,6 +2,7 @@ package lab.heisenbug.sandbox;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -17,5 +18,9 @@ public class SandboxApplication extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         LOGGER.info("building spring boot application.");
         return builder.sources(SandboxApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxApplication.class, args);
     }
 }
